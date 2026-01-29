0771e071df75498057b372ad1c553bf43e1883bd
Daniel Velez <velezdaniel516@gmail.com>
2026-01-29 15:45:13 -0500

    Switch action if statements to switch statement cases

--- scripts/script.js
+++ scripts/script.js
@@ -107,53 +107,6 @@ buttons.forEach(button => {
             display.textContent = firstInput + (operator ? ' ' + operator + ' ' + secondInput : '');
         }
 
-        //Create if statement for action
-        if(action === 'equals') {
-            //Check if user presses equal sign early
-            if(!firstInput || !secondInput || !operator) return;
-
-            //Turn first and second input into numbers
-            firstNumInput = +firstInput;
-            secondNumInput = +secondInput;
-
-            //Call the operation function in result variable
-            const result = operation(operator, firstNumInput, secondNumInput);
-
-            //Show result in display container
-            display.textContent = result;
-
-            if(typeof result === 'number') {
-                //Set result to first input, clear out second input and set operator to null
-                firstInput = result.toString();
-                secondInput = '';
-                operator = null;    
-            } else {
-                secondInput = '';
-                operator = null;
-            }
-        }
-
-        //Create if statement if action equals to delete
-        if(action === 'delete') {
-
-            if(!operator) {
-                firstInput = firstInput.slice(0,-1);
-                display.textContent = firstInput || '0';
-            } else {
-                secondInput = secondInput.slice(0,-1);
-                display.textContent = secondInput;
-            }   
-        }
-
-        //Create if statement if action equals to clear
-        if(action === 'clear') {
-            //Rest variables and display
-            firstInput = '';
-            secondInput = '';
-            operator = null;
-            display.textContent = '';
-        }
-
         //Create if statement for operator input
         if(operatorClicked !== undefined) {
 
@@ -174,6 +127,50 @@ buttons.forEach(button => {
             operator = operatorClicked;
 
             display.textContent = firstInput + " " + operator;
+        }        
+
+        //Create switch statement
+        switch(action) {
+            case 'equals':
+                //Check if user presses equal sign early
+                if(!firstInput || !secondInput || !operator) return;
+
+                //Turn first and second input into numbers
+                firstNumInput = +firstInput;
+                secondNumInput = +secondInput;
+
+                //Call the operation function in result variable
+                const result = operation(operator, firstNumInput, secondNumInput);
+
+                //Show result in display container
+                display.textContent = result;
+
+                if(typeof result === 'number') {
+                    //Set result to first input, clear out second input and set operator to null
+                    firstInput = result.toString();
+                    secondInput = '';
+                    operator = null;    
+                } else {
+                    secondInput = '';
+                    operator = null;
+                }    
+                break;
+            case 'delete':
+                if(!operator) {
+                    firstInput = firstInput.slice(0,-1);
+                    display.textContent = firstInput || '0';
+                } else {
+                    secondInput = secondInput.slice(0,-1);
+                    display.textContent = secondInput;
+                }   
+                break;
+            case 'clear':
+                //Rest variables and display
+                firstInput = '';
+                secondInput = '';
+                operator = null;
+                display.textContent = '';
+                break;
         }        
     });
 });
